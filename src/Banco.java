8fa0170e6ff06296a672f22eb62ae24f2498f856
Vinícius de Sousa Santana <sousavinicius@hotmail.com>
2022-06-14 11:32:46 -0300

    Initial structure for class Banco

--- src/Banco.java
+++ src/Banco.java
@@ -4,12 +4,18 @@ public class Banco {
 
     private String nome;
     ArrayList<ClientePessoaFisica> clientesPf = new ArrayList<>();
-    ArrayList<ClientePessoaFisica> clientesPj = new ArrayList<>();
+    ArrayList<ClientePessoaJuridica> clientesPj = new ArrayList<>();
     ArrayList<ContaCorrentePf> contasCorrentePf = new ArrayList<>();
     ArrayList<ContaPoupancaPf> contasPoupancaPf = new ArrayList<>();
     ArrayList<ContaInvestimentoPf> contasInvestimentoPf = new ArrayList<>();
     ArrayList<ContaCorrentePj> contasCorrentePj = new ArrayList<>();
     ArrayList<ContaInvestimentoPj> contasInvestimentoPj = new ArrayList<>();
+
+    // public TipoDeConta tipoDeConta;
+
+    enum TipoDeConta {
+        CONTA_CORRENTE, CONTA_POUPANCA, CONTA_INVESTIMENTO
+    }
 
     public Banco(String nome) {
         this.nome = nome;
@@ -19,7 +25,52 @@ public class Banco {
         return this.nome;
     }
 
-    public void abrirConta(Integer numeroConta, Integer numeroAgencia, ClientePessoaFisica cliente) {
+    // public TipoDeConta getTipoDeConta() {
+    // return tipoDeConta;
+    // }
+
+    // public void setTipoDeConta(TipoDeConta tipoDeConta) {
+    // this.tipoDeConta = tipoDeConta;
+    // }
+
+    public void abrirConta(Integer numeroConta, Integer numeroAgencia,
+            Cliente cliente, TipoDeConta tipoDeConta) {
+        if (cliente instanceof ClientePessoaFisica) {
+            clientesPf.add((ClientePessoaFisica) cliente);
+            switch (tipoDeConta) {
+            case CONTA_CORRENTE:
+                contasCorrentePf.add(new ContaCorrentePf(numeroConta,
+                        numeroAgencia, (ClientePessoaFisica) cliente));
+                break;
+            case CONTA_POUPANCA:
+                contasPoupancaPf.add(new ContaPoupancaPf(numeroConta,
+                        numeroAgencia, (ClientePessoaFisica) cliente));
+                break;
+            case CONTA_INVESTIMENTO:
+                contasInvestimentoPf.add(new ContaInvestimentoPf(numeroConta,
+                        numeroAgencia, (ClientePessoaFisica) cliente));
+                break;
+            }
+        } else {
+            if (cliente instanceof ClientePessoaJuridica) {
+                clientesPj.add((ClientePessoaJuridica) cliente);
+                switch (tipoDeConta) {
+                case CONTA_CORRENTE:
+                    contasCorrentePj.add(new ContaCorrentePj(numeroConta,
+                            numeroAgencia, (ClientePessoaJuridica) cliente));
+                    break;
+                case CONTA_POUPANCA:
+                    System.out.println(
+                            "Pessoa jurísica não pode abrir uma conta poupança.");
+                    break;
+                case CONTA_INVESTIMENTO:
+                    contasInvestimentoPj.add(
+                            new ContaInvestimentoPj(numeroConta, numeroAgencia,
+                                    (ClientePessoaJuridica) cliente));
+                    break;
+                }
+            }
+        }
 
     }
 
--- src/ContaCorrentePf.java
+++ src/ContaCorrentePf.java
@@ -2,9 +2,17 @@ public class ContaCorrentePf extends Conta {
 
     ClientePessoaFisica cliente;
 
-    public ContaCorrentePf(Integer numeroConta, Integer numeroAgencia, ClientePessoaFisica cliente) {
+    public ContaCorrentePf(Integer numeroConta, Integer numeroAgencia,
+            ClientePessoaFisica cliente) {
         super(numeroConta, numeroAgencia);
         this.cliente = cliente;
+    }
+
+    @Override
+    public String toString() {
+        return "ContaCorrentePf [numeroConta="
+                + super.getNumeroConta().toString() + ", numeroAgencia="
+                + super.getNumeroAgencia().toString() + "]";
     }
 
 }
